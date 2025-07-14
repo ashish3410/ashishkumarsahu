3563a49fc618dad445f0bc787bbe7a9b510a5e66
Ashish Sahu <148787104+ashish3410@users.noreply.github.com>
2025-07-14 13:18:53 +0530

    Update Home.jsx
    updated home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,40 @@
 import React from 'react'
+import { TypeAnimation } from 'react-type-animation';
 import { useNavigate } from 'react-router-dom'
 import Button from '../components/Button'
 function Home() {
-    const navigate=useNavigate()
+    const navigate = useNavigate()
 
-    const gotoAboutme=()=>{
+    const gotoAboutme = () => {
         navigate('/about')
     }
     return (
         <>
             <section style={{ backgroundImage: `url('https://i.ibb.co/MS8ZFKK/portfolio-Bg.png')` }} className='flex-col justify-center text-center h-full pt-48 z-10'>
                 <div>
-                    <h1 className='text-5xl font-bold p-4'>HEY, I'M ASHISH KUMAR SAHU</h1>
+                    <TypeAnimation
+                    className='text-4xl font-bold'
+                        sequence={[
+                            "HEY,I'M ASHISH KUMAR SAHU", 500,
+                            "I'M A SOFTWARE DEVELOPER", 500,
+                        ]}
+                        wrapper="span"
+                        speed={50}
+                        repeat={Infinity}
+                        cursor={true}
+                    />
                 </div>
                 <div className='p-4'>
                     <p className='text-xl'>
-                        A Passinate Web Developer building and managing Website and Web
-                        <br />Applications thats leads to the success of the overall products
+                        A Passionate Web Developer building and managing Website and Web
+                        <br />Applications, thats leads to the success of the overall products.
                     </p>
                 </div>
                 <div>
-                    <Button handleClick={gotoAboutme} label="ABOUT ME"/>
+                    <Button handleClick={gotoAboutme} label="ABOUT ME" />
                 </div>
             </section>
-        
+
         </>
     )
 }
